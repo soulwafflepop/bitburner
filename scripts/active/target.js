0c66002f990b0254b51c7432e276ff74bbd56176
soulwafflepop <kalei.sloat@icloud.com>
2024-11-16 15:02:20 -0800

    Update target.js

--- scripts/active/target.js
+++ scripts/active/target.js
@@ -3,9 +3,9 @@ import {
 } from './functions.js'
 
 
-export async function main(ns, depth = (ns.args[0] - 1)) {
+export async function main(ns, depth = ns.args[0]) {
   if (!(typeof depth == 'number')) {
     depth = 1;
   }
-  await target(ns, depth, false, true);
+  await target(ns, (depth + 1), false, true);
 }
