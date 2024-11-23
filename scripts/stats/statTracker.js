0ee174b915ae8fe10e7cc8858ff92cd41e057fc4
soulwafflepop <kalei.sloat@icloud.com>
2024-11-22 17:42:37 -0800

    Update statTracker.js

--- scripts/stats/statTracker.js
+++ scripts/stats/statTracker.js
@@ -1,36 +1,108 @@
-export async function main(ns, stat = ns.args[0]) {
+import {
+  abbr
+} from './functions.js'
+
+export async function main(ns) {
+  const end = Boolean(ns.args[0]); //0 for false, 1 for true
+  const hashes = Boolean(ns.args[1]); //0 for false, 1 for true
+  const hshRt = Boolean(ns.args[2]); //0 for false, 1 for true
+  const karma = Boolean(ns.args[3]); //0 for false, 1 for true
   const doc = eval('document');
   const hook0 = doc.getElementById('overview-extra-hook-0');
   const hook1 = doc.getElementById('overview-extra-hook-1');
-  var headers = [];
-  var values = [];
-  var hv = [headers, values];
-  if (stat == undefined) {
-    ns.exec("stats/hashCount.js", "home", 1, hook0, hook1, hv);
-    hv = ns.exec("stats/par/hashCount.js", "home", 1, hook0, hook1, hv);
 
-    ns.exec("stats/hashRate.js", "home", 1, hook0, hook1, hv);
-    hv = ns.exec("stats/par/hashRate.js", "home", 1, hook0, hook1, hv);
+  while (true) {
+    var moneyProduction = getProduction(ns);
+    if (ns.args[0] == undefined) { //runs hashes, hshRt, and karma
+      try {
+        const headers = [];
+        const values = [];
 
-    ns.exec("stats/karma.js", "home", 1, hook0, hook1, hv);
-    hv = ns.exec("stats/par/karma.js", "home", 1, hook0, hook1, hv);
+        headers.push("Hashes");
+        values.push("#" + abbr(ns.hacknet.numHashes().toPrecision(5)));
 
-  } else if (stat == 'hashes' || stat == 'hash' || stat == 'h') {
-    ns.exec("stats/hashCount.js", "home", 1, hook0, hook1, hv);
-    hv = ns.exec("stats/par/hashCount.js", "home", 1, hook0, hook1, hv);
+        headers.push("HshRt");
+        values.push("$" + abbr(moneyProduction.toPrecision(5)) + '/sec');
 
-  } else if (stat == 'rate' || stat == 'hrate' || stat == 'hr' || stat == "hashrate") {
-    ns.exec("stats/hashRate.js", "home", 1, hook0, hook1, hv);
-    hv = ns.exec("stats/par/hashRate.js", "home", 1, hook0, hook1, hv);
+        headers.push("Karma");
+        values.push(abbr(ns.heart.break()))
 
-  } else if (stat == 'karma' || stat == 'k' || stat == 'kar') {
-    ns.exec("stats/karma.js", "home", 1, hook0, hook1, hv);
-    hv = ns.exec("stats/par/karma.js", "home", 1, hook0, hook1, hv);
+        hook0.innerText = headers.join(" \n");
+        hook1.innerText = values.join("\n");
+      } catch (err) {
+        ns.print("ERROR: Update Skipped: " + String(err));
+      }
+    } else {
+      if (hashes == true) {
+        try {
+          const headers = [];
+          const values = [];
 
+          headers.push("Hashes");
+          values.push("#" + abbr(ns.hacknet.numHashes().toPrecision(5)));
+
+          hook0.innerText = headers.join(" \n");
+          hook1.innerText = values.join("\n");
+        } catch (err) {
+          ns.print("ERROR: Update Skipped: " + String(err));
+        }
+      }
+      if (hshRt == true) {
+        try {
+          const headers = [];
+          const values = [];
+
+          headers.push("HshRt");
+          values.push("$" + abbr(moneyProduction.toPrecision(5)) + '/sec');
+
+          hook0.innerText = headers.join(" \n");
+          hook1.innerText = values.join("\n");
+        } catch (err) {
+          ns.print("ERROR: Update Skipped: " + String(err));
+        }
+      }
+      if (karma == true) {
+        try {
+          const headers = [];
+          const values = [];
+
+          headers.push("Karma");
+          values.push(abbr(ns.heart.break()));
+
+          hook0.innerText = headers.join(" \n");
+          hook1.innerText = values.join("\n");
+        } catch (err) {
+          ns.print("ERROR: Update Skipped: " + String(err));
+        }
+      }
+    }
+    if (end == true) {
+      try {
+        const headers = [];
+        const values = [];
+
+        hook0.innerText = headers.join(" \n");
+        hook1.innerText = values.join("\n");
+      } catch (err) {
+        ns.print("ERROR: Update Skipped: " + String(err));
+      }
+      break;
+    }
+    await ns.sleep(250);
   }
-  if (stat == 'end' || stat == 'e' || stat == 'del' || stat == 'delete') {
-    ns.exec("stats/hashCount.js", "home", 1, hook0, hook1, hv, true);
-    ns.exec("stats/hashRate.js", "home", 1, hook0, hook1, hv, true);
-    ns.exec("stats/karma.js", "home", 1, hook0, hook1, hv, true);
+}
+
+function getProduction(ns) {
+  const ratioMoney = 1e6 / 4.00; //$1mil per 4 hashes
+  const currentNodes = ns.hacknet.numNodes();
+  // loops for all hacknet servers, adds productions together
+  var totalProduction = 0;
+  for (var i = 0; i < currentNodes; i++) {
+    let nodeStats = ns.hacknet.getNodeStats(i);
+    let product = nodeStats.production;
+    totalProduction = totalProduction + product;
   }
+
+  var moneyProduction = totalProduction * ratioMoney
+  return moneyProduction;
 }
