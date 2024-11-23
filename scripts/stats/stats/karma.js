442b9e7fba7250cc7bd2576034cead893135cc0a
soulwafflepop <kalei.sloat@icloud.com>
2024-11-22 16:54:23 -0800

    Create karma.js

---
+++ scripts/stats/stats/karma.js
@@ -0,0 +1,25 @@
+import {
+  abbr
+} from '/functions.js'
+
+export async function main(ns, remove = Boolean(ns.args[3])) {
+  var hook0 = ns.args[0];
+  var hook1 = ns.args[1];
+  var hv = ns.args[2];
+  var headers = hv[0];
+  var values = hv[1];
+
+  while (true) {
+    try {
+      headers.push("Karma");
+      values.push(abbr(ns.heart.break()));
+
+
+      hook0.innerText = headers.join(" \n");
+      hook1.innerText = values.join("\n");
+    } catch (err) {
+      ns.print("ERROR: Update Skipped: " + String(err));
+    }
+    await ns.sleep(250);
+  }
+}
